c7ce0c20934bc79d92acadd848ec39fd83932bc4
Hossein <hosseinkian1376676@gmail.com>
2020-05-21 07:21:18 +0330

    add main and its Contents

--- main.cpp
+++ main.cpp
@@ -7,7 +7,30 @@ using namespace std;
 int main() {
     MyBigNumber myBig1 = "111111";
     MyBigNumber myBig2 = "123456789";
-    MyBigNumber myBig3 = "000136776";
+    MyBigNumber myBig3 = "-000136776";
+    cout << ++myBig3 << endl;
+    cout << myBig3-- << endl;
+    cout << --myBig3 << endl;
+    cout << myBig3++ << endl;
+    cout << myBig3 << endl;
+    cout << myBig1 + myBig2 << endl;
+    cout << myBig2 - myBig1 << endl;
+    cout << myBig1 * myBig2 << endl;
+    cout << myBig1.power(5) << endl;
+    cout << myBig2(4 , 2) << endl;
+    cout << boolalpha << (myBig1 == myBig2) << endl;
+    cout << boolalpha << (myBig1 != myBig2) << endl;
+    cout << boolalpha << (myBig1 >= myBig2) << endl;
+    cout << boolalpha << (myBig1 <= myBig2) << endl;
+    cout << myBig1.multByOneDigit(3)<< endl;
+    cout << (myBig2<<3) << endl;
+    cout << (-myBig2) << endl;
+    try {
+        (myBig1>>8);
+    }
+    catch(invalid_argument e){
+        cout << e.what() << endl;
+    }
 
     return 0;
 }
